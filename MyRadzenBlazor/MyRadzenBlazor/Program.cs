db59af6be76f9f13eedd320201d0274d1f6f6453
Natanael <natanaelsantosbr@gmail.com>
2024-05-25 11:09:45 -0300

    Install Radzen

--- MyRadzenBlazor/MyRadzenBlazor/Program.cs
+++ MyRadzenBlazor/MyRadzenBlazor/Program.cs
@@ -1,7 +1,10 @@
 using MyRadzenBlazor.Client.Pages;
 using MyRadzenBlazor.Components;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRadzenComponents();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
