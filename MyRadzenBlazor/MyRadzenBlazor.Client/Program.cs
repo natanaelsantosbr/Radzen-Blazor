9e067fd0281c14a2059df2b2731af09311d772eb
Natanael <natanaelsantosbr@gmail.com>
2024-05-26 22:39:20 -0300

    Login usando o radzenlogin

--- MyRadzenBlazor/MyRadzenBlazor.Client/Program.cs
+++ MyRadzenBlazor/MyRadzenBlazor.Client/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MyRadzenBlazor.Client.Services;
 using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddRadzenComponents();
+
+builder.Services.AddSingleton<MockAuthService>();
 
 await builder.Build().RunAsync();
---
+++ MyRadzenBlazor/MyRadzenBlazor.Client/Services/MockAuthService.cs
@@ -0,0 +1,10 @@
+﻿namespace MyRadzenBlazor.Client.Services
+{
+    public class MockAuthService
+    {
+        public bool Login(string username, string password)
+        {
+            return username == "admin" && password == "123456789";
+        }
+    }
+}
