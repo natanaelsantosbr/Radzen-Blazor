41ffac7a4a4e12c9a294deedcbdf283650ea49bd
Natanael <natanaelsantosbr@gmail.com>
2024-05-28 19:53:51 -0300

    Create Product

---
+++ MyRadzenBlazor/MyRadzenBlazor.Client/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyRadzenBlazor.Client.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
